bb8df5685702f85cda71af5edfdc75b1890c0840
Vitor Sanches <viisanches98@gmail.com>
2024-01-15 14:23:41 -0300

    deploy: migrate to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,32 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+import compress from 'astro-compress';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-
-import compress from "astro-compress";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap(), react(), compress({ Image: false })],
+  image: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.ytimg.com',
+      },
+    ],
+  },
   output: 'server',
-  adapter: netlify({
-    imageCDN: true
+  adapter: vercel({
+    webAnalitics: { enabled: true },
+    maxDuration: 10,
+    imageService: true,
   }),
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es', 'pt'],
     routing: {
-      prefixDefaultLocale: false
-    }
-  }
+      prefixDefaultLocale: false,
+    },
+  },
 });
