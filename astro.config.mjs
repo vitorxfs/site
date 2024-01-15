703c7ea10e56fe780770f93a9a3b67606eb19b5d
Vitor Sanches <viisanches98@gmail.com>
2024-01-15 02:02:05 -0300

    perf: add astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,20 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), react()],
+  integrations: [tailwind(), sitemap(), react(), compress({ Image: false })],
   output: 'server',
   adapter: netlify({
-    imageCDN: true,
+    imageCDN: true
   }),
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es', 'pt'],
     routing: {
-      prefixDefaultLocale: false,
-    },
-  },
+      prefixDefaultLocale: false
+    }
+  }
 });
