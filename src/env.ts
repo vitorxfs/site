74af71c212c79b1797d273e4b7e8ca594e4f714c
Vitor <viisanches98@gmail.com>
2024-01-16 01:55:59 -0300

    feat(seo): add seo metatags

--- src/env.ts
+++ src/env.ts
@@ -4,3 +4,6 @@ export const MEDIUM_FEED_BASE_URL =
   import.meta.env.MEDIUM_FEED_BASE_URL || 'https://medium.com/feed';
 export const GITHUB_KEY = import.meta.env.GITHUB_KEY || '';
 export const GITHUB_USER = import.meta.env.GITHUB_USER || 'vitorxfs';
+export const TWITTER_HANDLE = import.meta.env.TWITTER_HANDLE || '@vitorsnx';
+export const CANONICAL_URL = import.meta.env.CANONICAL_URL || 'https://vitorsanches.com';
+export const SITE_BASE_URL = import.meta.env.SITE_BASE_URL || 'https://vitorsanches.com';
--- src/i18n/meta.ts
+++ src/i18n/meta.ts
@@ -1,11 +1,14 @@
 export const meta = {
   pt: {
-    description: 'Desenvolvedor front-end',
+    description:
+      'Sou desenvolvedor web com foco no front end. Possuo experiência com react, node, ux/ui e no momento estou me aventurando com astro.build e performance web.',
   },
   es: {
-    description: 'Desarrollador front-end',
+    description:
+      'Soy desarrollador web nfocado en el front end. Tengo experiencia con react, node, ux/ui y actualmente estoy explorando el astro.build y la web performance.',
   },
   en: {
-    description: 'Front-end developer',
+    description:
+      "I'm a web developer focused in the front end. I have experience with react, node, ux/ui and lately I have been exploring astro.build and web performance.",
   },
 } as const;
