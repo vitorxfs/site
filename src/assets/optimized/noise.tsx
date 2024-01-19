84bb30611eb206d9767e4ae8dfba772855ed50b2
Vitor Sanches <viisanches98@gmail.com>
2024-01-19 12:15:34 -0300

    feat(performance): add preload of background image

---
+++ src/assets/optimized/noise.tsx
@@ -0,0 +1,23 @@
+import { getImage } from 'astro:assets';
+import recycledImg from '../img/recycled-texture.png';
+import noiseImg from '../img/noise.png';
+
+let noiseLightSrc: string | undefined = undefined;
+let noiseDarkSrc: string | undefined = undefined;
+
+export const getNoiseLightSrc = async (): Promise<string> => {
+  if (!noiseLightSrc) {
+    console.log('rodou');
+    noiseLightSrc = (await getImage({ src: recycledImg, format: 'webp', prerender: true })).src;
+  }
+
+  return noiseLightSrc;
+};
+
+export const getNoiseDarkSrc = async (): Promise<string> => {
+  if (!noiseDarkSrc) {
+    noiseDarkSrc = (await getImage({ src: noiseImg, format: 'webp', prerender: true })).src;
+  }
+
+  return noiseDarkSrc;
+};
