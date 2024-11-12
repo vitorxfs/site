cd9326d2dd2bca2955e37bf89efe70ac091a4d49
Vitor <viisanches98@gmail.com>
2024-11-12 17:54:26 -0300

    fix: sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://vitorsanches.com',
   integrations: [
     tailwind(),
     sitemap(),
